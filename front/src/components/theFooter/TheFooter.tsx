98ced81402f323433f61bd8da81a7c05fd080703
Viktor <viktor4strlsov@gmail.com>
2024-06-04 02:11:10 +0600

    main

--- front/src/components/theFooter/TheFooter.tsx
+++ front/src/components/theFooter/TheFooter.tsx
@@ -1,54 +1,32 @@
 import styles from './TheFooter.module.scss'
-import CarIcon from '@/components/theFooter/icons/CarIcon'
-import ClassssIcon from '@/components/theFooter/icons/ClassssIcon'
-import ProsentIcon from '@/components/theFooter/icons/ProsentIcon'
-import FileBlockIcon from '@/components/theFooter/icons/FileBlockIcon'
-import Logo from '@/components/icons/Logo'
 import Facebook from '@/components/theFooter/icons/facebook'
-import Twiter from '@/components/theFooter/icons/twiter'
 import Insta from '@/components/theFooter/icons/insta'
 import Youtybe from '@/components/theFooter/icons/youtybe'
-import Acounti from '@/components/theFooter/icons/acounti'
+import LogoFooter from "@/components/theFooter/icons/logoFooter";
 
 const TheFooter = () => {
 	return (
 		<footer className={styles.footer}>
 			<div className={styles.contentYslug}>
 				<div className={styles.yslygi}>
-					<div className={styles.blockYsl}>
-						<div><CarIcon /></div>
-						<div className={styles.text}>Бесплатная доставка</div>
+					<div className={styles.infoFooter}>
+						<div><LogoFooter/></div>
+						<p>4517 Washington Ave. <br/> Manchester, Kentucky 39495</p>
+						<ul className={styles.links}>
+							<li><Facebook/></li>
+							<li><Insta/></li>
+							<li><Youtybe/></li>
+						</ul>
 					</div>
-					<div className={styles.blockYsl}>
-						<div><ClassssIcon /></div>
-						<div className={styles.text}>Экспертное обслуживание клиентов</div>
-					</div>
-					<div className={styles.blockYsl}>
-						<div><ProsentIcon /></div>
-						<div className={styles.text}>Удивительная ценность</div>
-					</div>
-					<div className={styles.blockYsl}>
-						<div><FileBlockIcon /></div>
-						<div className={styles.text}>Безопасная оплата</div>
-					</div>
-				</div>
-			</div>
-			<div className={styles.blockAcounti}>
-				<div className={styles.follow}>
-					<div className={styles.followText}>Follow Us</div>
-					<ul className={styles.siti}>
-						<li><Facebook /></li>
-						<li><Twiter /></li>
-						<li><Insta /></li>
-						<li><Youtybe /></li>
+					<ul className={styles.tel}>
+						<li className={styles.textTel}>Телефон</li>
+						<li>+996 555 555 555</li>
+						<li>+996 555 555 555</li>
+						<li>+996 555 555 555</li>
 					</ul>
 				</div>
-				<div className={styles.logo}><Logo /></div>
-				<div className={styles.follow}>
-					<div className={styles.followText}>We Accept</div>
-					<div><Acounti /></div>
-				</div>
 			</div>
+
 		</footer>
 	)
 }
